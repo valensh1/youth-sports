608a214f1bf490e15451028de15ee4d976f170c0
Shaun Valentine <valensh1@yahoo.com>
2021-11-07 21:32:43 -0800

    Change to index page

--- client/src/views/Hockey/Roster.js
+++ client/src/views/Hockey/Roster.js
@@ -44,7 +44,7 @@ const Roster = () => {
           return (
             <tbody key={player?._id}>
               <tr className="index__player">
-                <td>
+                <td className="index__player-pic-name">
                   <Link
                     className="index__player-link"
                     to={`/hockeyPlayers/${player?._id}`}
