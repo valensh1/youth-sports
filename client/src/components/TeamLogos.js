f8c544335af6e1725d894aed0dd76984d2147da7
Shaun Valentine <valensh1@yahoo.com>
2022-01-03 12:06:19 -0800

    Trying to fix team logos deployement issues on Heroku

--- client/src/components/TeamLogos.js
+++ client/src/components/TeamLogos.js
@@ -23,8 +23,6 @@ function TeamLogos({ team, logo }) {
     })();
   }, []);
 
-  console.log(teamLogo);
-
   return (
     <div>
       {/* <img src={`./img/Team-Logos/Hockey/${logoToDisplay()}`} alt="" /> */}
